f2be8ca4b8a5a6d7ced1d864366bcc797de51450
Valentin Mihov <valentin.mihov@gmail.com>
2009-09-15 11:36:24 +0300

    Some tweaks of the chroot functionality

--- runner.pl
+++ runner.pl
@@ -2,6 +2,18 @@
 use Getopt::Long;
 use BSD::Resource;
 
+if(@ARGV == 0) {
+  print "Usage: runner.pl <options> <command>\n";
+  print "Options: \n";
+  print "  --mem <bytes> - The amount of memory allowed in bytes\n";
+  print "  --time <seconds> - The amount of CPU time allowed in seconds\n";
+  print "  --procs <processes> - The number of processes allowed during the execution of the command\n";
+  print "  --files <files> - The number of allowed open files\n";
+  print "  --user <username> - The user with which to execute the command\n";
+  print "  --root <path_to_root> - A directory in which to chroot the command\n";
+  exit 0;
+}
+
 GetOptions("mem:i"   => \$mem,
            "time:i"  => \$time,
            "procs:i" => \$procs,
@@ -14,18 +26,8 @@ setrlimit(RLIMIT_NPROC, $procs, $procs) or die "Cannot set NPROC limit" if $proc
 setrlimit(RLIMIT_OFILE, $files, $files) or die "Cannot set OFILE limit" if $files;
 setrlimit(RLIMIT_CPU, $time, $time) or die "Cannot set CPU limit" if $time;
 
-chroot $root or die "Cannot chroot to $root" if $root;
+chroot($root) or die "Cannot chroot to $root" if $root;
 
-if(@ARGV == 0) {
-  print "Usage: runner.pl <options> <command>\n";
-  print "Options: \n";
-  print "  --mem <bytes> - The amount of memory allowed in bytes\n";
-  print "  --time <seconds> - The amount of CPU time allowed in seconds\n";
-  print "  --procs <processes> - The number of processes allowed during the execution of the command\n";
-  print "  --files <files> - The number of allowed open files\n";
-  print "  --user <username> - The user with which to execute the command\n";
-  print "  --root <path_to_root> - A directory in which to chroot the command\n";
-  exit 0;
-}
+$> = getpwnam($user) if $user;
 
 exec @ARGV
