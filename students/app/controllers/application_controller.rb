b88920d228cdfc3d59f284eeacbe729c430cd989
Valentin Mihov <valentin.mihov@gmail.com>
2009-10-08 23:48:57 +0300

    Added a bulgarian locale to the app. This is going to translate all the error messages in bugarian

--- students/app/controllers/application_controller.rb
+++ students/app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   include AuthenticatedSystem
   filter_parameter_logging :password
+
+  I18n.locale = :bg
+
 end
