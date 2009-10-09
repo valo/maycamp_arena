ff87a1b8b7e9ecb56af69d1fbdc032a8965680f5
Valentin Mihov <valentin.mihov@gmail.com>
2009-10-09 13:51:17 +0300

    Unpacked the rack gem
    Former-commit-id: 40b40b77a759cac77e53eb2885e52ac3b1938ac4

--- students/config/environment.rb
+++ students/config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'rubyzip', :lib => "zip/zip"
   config.gem 'andand'
+  config.gem 'rack', :version => '1.0.0'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
