8f41eb2a2ce57947fa10d87d402ba002b39a57f2
Slavi Marinov <slavi.marinov@gmail.com>
2009-10-15 15:08:14 +0300

    On sync, recreate the sets/ folder so that when the tests for a problem are renamed, it doesn't end up with 2x tests.

--- students/lib/sets_sync.rb
+++ students/lib/sets_sync.rb
@@ -4,6 +4,11 @@ class SetsSync
   class << self
     def sync_sets(grader_app_url)
       puts "Syncing tests with #{grader_app_url}..."
+      
+      # Recreate the sets directory to make sure no files are left from the old copy.
+      FileUtils.rm_rf $config[:sets_root]
+      FileUtils.mkdir_p $config[:sets_root]
+
       URI.join(grader_app_url, 'main/download_tests').open do |remote_zip|
         File.open(File.join($config[:sets_root], "sets.zip"), "w") do |f|
           while (data = remote_zip.read(1024)) do
