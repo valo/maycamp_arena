5b6ced23fd6200793bb252240800e1b688a5b548
Valentin Mihov <valentin.mihov@gmail.com>
2009-10-27 21:38:16 +0200

    Fixed a migration issue

--- students/db/migrate/20090911200430_add_admin_user.rb
+++ students/db/migrate/20090911200430_add_admin_user.rb
@@ -2,9 +2,10 @@ class AddAdminUser < ActiveRecord::Migration
   def self.up
     u = User.new(:login => 'root', 
                  :name => "The admin", 
-                 :email => "valentin.mihov@gmail.com",
-                 :password => "123123",
-                 :password_confirmation => "123123")
+                 :email => "valentin.mihov@gmail.com")
+
+    u.unencrypted_password = "123123"
+    u.unencrypted_password_confirmation = "123123"
     u.admin = true
     u.save!
   end
