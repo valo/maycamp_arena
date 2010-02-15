ec04019fd5b0dfa0c11695b89e28f5c8470cfe92
Valentin Mihov <valentin.mihov@gmail.com>
2010-02-16 00:37:46 +0200

    Added a reports framework and a submits by day report
    Former-commit-id: e551e4bec52c92a7063dfc32ae13af278b29974c

--- students/config/routes.rb
+++ students/config/routes.rb
@@ -67,6 +67,8 @@ ActionController::Routing::Routes.draw do |map|
         }
       end
     end
+
+    admin.resource :reports
   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
