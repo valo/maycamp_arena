6fb39202df7240f51f577030b409f7506805e1c2
Valentin Mihov <valentin.mihov@gmail.com>
2010-03-23 01:55:47 +0200

    Some deployment recepies
    Former-commit-id: 863869196916119835a849ad4c6b770044e5964d

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,49 @@
-set :application, "Maycamp Arena"
-set :repository,  "none"
+set :application, "sarena.maycamp.com"
+set :repository,  "git://github.com/valo/spoj0.git"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, "v0.2RC"
+set :user, "maycamp"
+set :deploy_to, "/home/maycamp/#{application}"
+set :use_sudo, false
+set :database_name, "sarena"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, application                          # Your HTTP server, Apache/etc
+role :app, application                          # This may be the same as your `Web` server
+role :db,  application, :primary => true # This is where Rails migrations will run
+
+namespace :db do
+  task :backup, :roles => :db do
+    
+  end
+end
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :db do
+  task :symlink, :except => { :no_release => true } do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  
+  task :sync do
+    run "mysqldump spoj0_prod -h mysql.maycamp.com -u admin_spoj0 -pparola | mysql sarena -u admin_staging -pparola -h mysql.maycamp.com"
+  end
+  
+  task :backup do
+    backup_file = "#{shared_path}/backup_#{Time.now.utc.strftime("%Y%m%d%H%M%S")}"
+    run "mysqldump spoj0_prod -h mysql.maycamp.com -u admin_spoj0 -pparola > #{backup_file}"
+    get backup_file, "backup"
+  end
+end
+
+after "deploy:finalize_update", "db:symlink"
