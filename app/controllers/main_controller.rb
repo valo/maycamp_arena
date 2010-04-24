3a585b7d4614c571fc120cde9ae3121a3cb027e9
Anton Dimitrov <anton_r_dimitrov@yahoo.com>
2010-04-25 07:07:24 +0800

    Fixed two issues with additional SQL statement and restriction of access for non-admin users. Also changed the view for problems' categories. Now the categories names are only visible and the user must click  on the names in order to get a list of problems for this category. Removed an obsolete file.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -80,6 +80,10 @@ class MainController < ApplicationController
     @categories = Category.find(:all).sort {|x, y| x.name.casecmp(y.name) }
   end
 
+  def show_category_problems
+    @category = Category.find(params[:id])
+  end
+
   private
     def calc_rankings(options = {})
       rankings = User.generate_ranklist(options).map do |row|
