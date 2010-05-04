b5022e3d27c2cd99826019bf9a95cc061e210023
Valentin Mihov <valentin.mihov@gmail.com>
2010-05-04 12:18:57 +0300

    Fixed the validation of the email during sending of the messages
    Former-commit-id: af0c00d3ee62c9bc53e41e6b3c4d39d90f391a9a

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,7 +6,7 @@ class Message < ActiveRecord::Base
       self.emails_sent = User.first.email
       UserMails.deliver_message([User.first], self)
     else
-      self.emails_sent = User.all.map(&:email).select { |e| e.include?("@") }.join(', ')
+      self.emails_sent = User.all.map(&:email).select { |e| e =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }.join(', ')
       UserMails.deliver_message(User.all, self)
     end
     save
