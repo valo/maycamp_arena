0c995ec17888e1d27417b579b041c6ee66349a28
Valentin Mihov <valentin.mihov@gmail.com>
2010-06-16 11:41:16 +0300

    Fixed a bug in the new mail code.
    -
    
    
    Former-commit-id: 4ca2c8bf91c5cdc903522c921b2b52d00f4a611b

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,12 +6,12 @@ class Message < ActiveRecord::Base
       self.emails_sent = User.first.email
       UserMails.deliver_message([User.first], self)
     else
-      emails_left = emails_to_send = User.all.map(&:email).select { |e| e =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }.join(', ')
+      emails_left = emails_to_send = User.all.map(&:email).select { |e| e =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i }
       
       # Send the emails in chunks of 10. We don't want to overload the mail
       # server
       while !emails_left.empty?
-        self.emails_sent = emails_left[0, 10]
+        self.emails_sent = emails_left[0, 10].join(', ')
         UserMails.deliver_message(User.all, self)
         
         emails_left = emails_left[10..-1]
