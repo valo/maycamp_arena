55ad3097fa3c5901bb3b62bd77638e772e4da021
Valentin Mihov <valentin.mihov@gmail.com>
2010-10-15 11:29:36 -0700

    Merging migrations

--- db/migrate/20100522150753_convert_source_code_to_blob.rb
+++ db/migrate/20100522150753_convert_source_code_to_blob.rb
@@ -1,9 +1,7 @@
 class ConvertSourceCodeToBlob < ActiveRecord::Migration
   def self.up
-    change_column :runs, :source_code, :binary
   end
 
   def self.down
-    change_column :runs, :source_code, :text
   end
 end
