0ee5c63f2ab37b698971707dce1d93a4338ee2b4
Valentin Mihov <valentin.mihov@gmail.com>
2010-10-15 11:39:09 -0700

    Robust hoptoad config

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,7 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = '8b635d0b6d32d19dd6640f3f0d0b0ce4'
+begin
+  HoptoadNotifier.configure do |config|
+    config.api_key = '8b635d0b6d32d19dd6640f3f0d0b0ce4'
+  end
+rescue NameError
+  puts "Hoptoad is disabled"
 end
