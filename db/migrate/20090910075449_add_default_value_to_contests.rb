85ad2d0f9de95d1ca899352dc140190297aedb32
Valentin Mihov <valentin.mihov@gmail.com>
2010-10-15 11:12:15 -0700

    Fixing uncompatible migration

--- db/migrate/20090910075449_add_default_value_to_contests.rb
+++ db/migrate/20090910075449_add_default_value_to_contests.rb
@@ -1,9 +1,9 @@
 class AddDefaultValueToContests < ActiveRecord::Migration
   def self.up
-    change_column_default :contests, :show_sources, false
+    change_column_default :contests, :show_sources, '0'
   end
 
   def self.down
-    change_column_default :contests, :show_sources, false
+    change_column_default :contests, :show_sources, '0'
   end
 end
