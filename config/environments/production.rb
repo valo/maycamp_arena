845c74551ab14b3e9e3ea1156d42e527fa20dc3c
Valentin Mihov <valentin.mihov@gmail.com>
2011-05-01 23:42:34 +0300

    Adding the syslogger gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ MaycampArena::Application.configure do
   # config.log_level = :debug
 
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  config.logger = SyslogLogger.new
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
