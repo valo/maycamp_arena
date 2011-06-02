e3eda31eec47ab176156bfdcef5f58150dc3fce6
Valentin Mihov <valentin.mihov@gmail.com>
2011-06-02 11:00:49 +0300

    Adding unicorn config and deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,4 @@
+
 DEPLOY_STAGE = ENV['STAGE'] || "staging"
 
 load File.join(File.dirname(__FILE__), "deploys", DEPLOY_STAGE)
@@ -12,6 +13,30 @@ set :use_sudo, false
 set :scm, :git
 set :git_enable_submodules, 1
 
-role :web, "89.252.247.77"                          # Your HTTP server, Apache/etc
-role :app, "89.252.247.77"                          # This may be the same as your `Web` server
-role :db,  "89.252.247.77", :primary => true # This is where Rails migrations will run
+role :web, "maycamp.com"                          # Your HTTP server, Apache/etc
+role :app, "maycamp.com"                          # This may be the same as your `Web` server
+role :db,  "maycamp.com", :primary => true # This is where Rails migrations will run
+
+set :rails_env, :production
+set :unicorn_binary, "bundle exec unicorn_rails"
+set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do 
+    run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
+  end
+  task :stop, :roles => :app, :except => { :no_release => true } do 
+    run "kill `cat #{unicorn_pid}`"
+  end
+  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+    run "kill -s QUIT `cat #{unicorn_pid}`"
+  end
+  task :reload, :roles => :app, :except => { :no_release => true } do
+    run "kill -s USR2 `cat #{unicorn_pid}`"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    stop
+    start
+  end
+end
