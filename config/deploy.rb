9e810be1486ddb805ef205ec0b3cff862e197409
Valentin Mihov <valentin.mihov@gmail.com>
2011-06-02 13:08:00 +0300

    Restructured the capistrano recipes and made the unicorn to serve the static content
    Former-commit-id: 185cc076ebb4197f4a225b1dbf8bb7943e49d81a

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,22 +21,3 @@ set :rails_env, :production
 set :unicorn_binary, "bundle exec unicorn_rails"
 set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
-
-namespace :deploy do
-  task :start, :roles => :app, :except => { :no_release => true } do 
-    run "cd #{current_path} && #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
-  end
-  task :stop, :roles => :app, :except => { :no_release => true } do 
-    run "kill `cat #{unicorn_pid}`"
-  end
-  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "kill -s QUIT `cat #{unicorn_pid}`"
-  end
-  task :reload, :roles => :app, :except => { :no_release => true } do
-    run "kill -s USR2 `cat #{unicorn_pid}`"
-  end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    stop
-    start
-  end
-end
