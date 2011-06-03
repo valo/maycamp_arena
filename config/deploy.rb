8b3afc7513a1193d6f3868c020cc4e7fa3ea1c88
Valentin Mihov <valentin.mihov@gmail.com>
2011-06-03 09:34:05 +0300

    Added a default stage.
    Added cap task for creating the config shared files and the sets folder.
    
    Started a cap task for installation of RVM
    
    
    Former-commit-id: 6c066ce88e482c142f97b952c4fc980942ece3a6

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,13 @@
+DEPLOY_STAGE = ENV['STAGE'] || "default"
 
-DEPLOY_STAGE = ENV['STAGE'] || "staging"
+# This must be configured for each system maycamp is installed to
+set :user, "maycamp"
+set :home_path, "/home/maycamp"
+set :port, 22
 
 load File.join(File.dirname(__FILE__), "deploys", DEPLOY_STAGE)
 load File.join(File.dirname(__FILE__), "deploys/recipies")
 
-set :repository, "git://github.com/valo/maycamp_arena.git"
-set :user, "maycamp"
-set :home_path, "/home/maycamp"
-set :deploy_to, File.join(home_path, application)
-set :deploy_via, :remote_cache
-set :use_sudo, false
-set :scm, :git
-set :git_enable_submodules, 1
-
 role :web, "maycamp.com"                          # Your HTTP server, Apache/etc
 role :app, "maycamp.com"                          # This may be the same as your `Web` server
 role :db,  "maycamp.com", :primary => true # This is where Rails migrations will run
-
-set :rails_env, :production
-set :unicorn_binary, "bundle exec unicorn_rails"
-set :unicorn_config, "#{current_path}/config/unicorn.conf.rb"
-set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
--- config/deploys/staging.rb
+++ config/deploys/staging.rb
@@ -1,3 +1,2 @@
 set :application, "sarena.maycamp.com"
-
 set :branch, "master"
