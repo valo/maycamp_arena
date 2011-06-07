518fd71c26aac761230c4ba20de13261eb20f0d1
Valentin Mihov <valentin.mihov@gmail.com>
2011-06-08 01:27:49 +0300

    Fixed run ordering issues.
    Added cap deploys for grader.valentinmihov.com and for the judge.openfmi.net
    
    
    Former-commit-id: 4abaaed8ef10639d59fa2e5970bf3ed508c4b0a9

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ set :user, "maycamp"
 set :home_path, "/home/maycamp"
 set :port, 22
 
-load File.join(File.dirname(__FILE__), "deploys", DEPLOY_STAGE)
-load File.join(File.dirname(__FILE__), "deploys/recipies")
-
 role :web, "maycamp.com"                          # Your HTTP server, Apache/etc
 role :app, "maycamp.com"                          # This may be the same as your `Web` server
 role :db,  "maycamp.com", :primary => true # This is where Rails migrations will run
+
+load File.join(File.dirname(__FILE__), "deploys", DEPLOY_STAGE)
+load File.join(File.dirname(__FILE__), "deploys/recipies")
