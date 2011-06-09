92ee52503272f5ce9791f51091faeb02bf0c9c76
Valentin Mihov <valentin.mihov@gmail.com>
2011-06-10 01:29:57 +0300

    Removed the ezgraphics plugin. Replaced with highcharts.
    Former-commit-id: 8cff5b972ca6740a8b9057212fe4330e5eb7bdf4

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,43 +1,19 @@
 class Admin::ReportsController < Admin::BaseController
   def show
-    @daily_submits_report = Ezgraphix::Graphic.new(
-                              :w => 900, 
-                              :color => "00FF00",
-                              :div_name => "daily_submits_report",
-                              :c_type => "line",
-                              :precision => 0,
-                              :rotate => "1",
-                              :caption => "Брой събмити на ден - Последни 3 седмици",
-                              :data => Run.count(:id, 
-                                                 :conditions => ["created_at > ?", 3.weeks.ago.to_s(:db)], 
-                                                 :select => "id",
-                                                 :group => "DATE_FORMAT(created_at, '%m/%d')"))
+    @daily_submits_report = Run.count(:id, 
+                                      :conditions => ["created_at > ?", 3.weeks.ago.to_s(:db)], 
+                                      :select => "id",
+                                      :group => "DATE_FORMAT(created_at, '%m/%d')")
 
-    @total_submits_report = Ezgraphix::Graphic.new(
-                            :w => 900, 
-                            :div_name => "total_submits_report",
-                            :c_type => "area2d",
-                            :names => "0",
-                            :precision => 0,
-                            :caption => "Брой събмити на ден",
-                            :data => Run.count(:id, 
-                                               :select => "id",
-                                               :group => "DATE_FORMAT(created_at, '%Y/%m/%d')"))
+    @total_submits_report = Run.count(:id, 
+                                      :select => "id",
+                                      :group => "DATE_FORMAT(created_at, '%Y/%m/%d')")
 
-    data = Run.connection.select_all("SELECT count(*) AS count_all, contests.name AS contests_name 
+    @contest_submit_report = Run.connection.select_all("SELECT count(*) AS count_all, contests.name AS contests_name 
                                       FROM `runs`
                                       INNER JOIN `problems` ON `problems`.id = `runs`.problem_id
                                       INNER JOIN `contests` ON `contests`.id = `problems`.contest_id
                                       GROUP BY contests.id
-                                      ORDER BY contests.created_at")
-    @contest_submit_report = Ezgraphix::Graphic.new(
-                              :w => 900,
-                              :div_name => "contest_submit_report",
-                              :c_type => "col2d",
-                              :names => "0",
-                              :precision => 0,
-                              :caption => "Брой събмити по състезания",
-                              :f_number_scale => 0,
-                              :data => data.inject([]) { |sum, a| sum << [a["contests_name"], a["count_all"]] })
+                                      ORDER BY contests.created_at").inject([]) { |sum, a| sum << [a["contests_name"], a["count_all"]] }
   end
 end
