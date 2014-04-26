4c3e6092a5c60ceb06e3f3f065abfafc9f5d0fd6
Valentin Mihov <valentin.mihov@gmail.com>
2014-04-27 01:16:24 +0300

    Fixing the mailer default options
    Former-commit-id: d286663d3a06666fa98911b3cefe5ddbab9ec6e0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,5 @@ MaycampArena::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.action_mailer.default :content_type =>  "text/html", :url_options => { :host => "arena.maycamp.com" }
+  config.action_mailer.default_url_options = { :host => "arena.maycamp.com" }
 end
