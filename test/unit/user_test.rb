3e7358535c4601a9252db23bf63675ba129194f9
Valentin Mihov <valentin.mihov@gmail.com>
2015-02-28 18:17:38 +0200

    Fixing the minitest tests to support roles

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -8,11 +8,9 @@ class UserTest < ActiveSupport::TestCase
   test "only non admin contester can compete" do
     user = User.new
     assert user.participates_in_contests?
-    user.admin = 1
+    user.role = User::ADMIN
     assert (not user.participates_in_contests?)
-    user.contester = 0
-    assert (not user.participates_in_contests?)
-    user.admin = 0
+    user.role = User::COACH
     assert (not user.participates_in_contests?)
   end
 end
