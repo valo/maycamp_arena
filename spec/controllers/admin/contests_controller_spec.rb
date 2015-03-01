8cab01adc152abb2dcf0c84b67cab6e92b1bf811
Valentin Mihov <valentin.mihov@gmail.com>
2015-03-01 12:18:01 +0200

    DRYed up the contests controller specs

--- spec/controllers/admin/contests_controller_spec.rb
+++ spec/controllers/admin/contests_controller_spec.rb
@@ -11,9 +11,7 @@ describe Admin::ContestsController do
     end
   end
 
-  context "with an admin user" do
-    let(:user) { create(:user, role: User::ADMIN) }
-
+  shared_examples "accessed by authorized user" do
     describe "#index" do
       before do
         sign_in(user)
@@ -25,18 +23,16 @@ describe Admin::ContestsController do
     end
   end
 
+  context "with an admin user" do
+    let(:user) { create(:user, role: User::ADMIN) }
+
+    it_behaves_like "accessed by authorized user"
+  end
+
 
   context "with a coach user" do
     let(:user) { create(:user, role: User::COACH) }
 
-    describe "#index" do
-      before do
-        sign_in(user)
-
-        get :index
-      end
-
-      it { is_expected.to respond_with(:success) }
-    end
+    it_behaves_like "accessed by authorized user"
   end
 end
