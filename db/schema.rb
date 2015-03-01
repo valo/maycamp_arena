b0b64708ac8223f51b32c8dfac2c4dc0f96e51e3
Valentin Mihov <valentin.mihov@gmail.com>
2015-03-01 18:54:46 +0200

    Make all users coaches except the root user
    Do not show the download sources link if you don't have access

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150228142734) do
+ActiveRecord::Schema.define(version: 20150301164809) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
