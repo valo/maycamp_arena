246063cbdb0189e0c9f8c62cdb396696a68e98d8
Valentin Mihov <valentin.mihov@gmail.com>
2015-03-01 18:29:52 +0200

    Added specs for the non-REST actions in the problems controller

--- spec/controllers/admin/problems_controller_spec.rb
+++ spec/controllers/admin/problems_controller_spec.rb
@@ -8,6 +8,86 @@ describe Admin::ProblemsController do
 
       it { is_expected.to redirect_to(new_session_path) }
     end
+
+    describe "#new" do
+      before { get :new, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#create" do
+      let(:problem_params) { attributes_for(:problem) }
+      before { post :create, contest_id: contest.id, problem: problem_params }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#edit" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :edit, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#update" do
+      let(:problem) { create(:problem, contest: contest) }
+      let(:new_name) { "test test test" }
+      before { put :update, id: problem.id, problem: { name: new_name }, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#destroy" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { delete :destroy, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#purge_files" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :purge_files, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#purge_files" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :purge_files, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#upload_file" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :upload_file, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#do_upload_file" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { post :do_upload_file, id: problem.id, contest_id: contest.id, tests: { file: fixture_file_upload("archive.zip")} }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#download_file" do
+      let(:problem) { create(:problem, contest: contest) }
+      let(:file_name) { "test.in" }
+      before do
+        get :download_file, id: problem.id, contest_id: contest.id, file: file_name
+      end
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+    describe "#compile_checker" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :compile_checker, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
   end
 
   shared_examples "accessed by authorized user" do
@@ -60,6 +140,47 @@ describe Admin::ProblemsController do
       it "deletes the problem" do
         expect(Contest.find_by_id(problem.id)).to be_nil
       end
+    end
+
+    describe "#purge_files" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :purge_files, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(admin_contest_problem_path(contest, problem)) }
+    end
+
+    describe "#upload_file" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :upload_file, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to respond_with(:success) }
+    end
+
+    describe "#do_upload_file" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { post :do_upload_file, id: problem.id, contest_id: contest.id, tests: { file: fixture_file_upload("archive.zip")} }
+
+      it { is_expected.to redirect_to(admin_contest_problem_path(contest, problem)) }
+    end
+
+    describe "#download_file" do
+      let(:problem) { create(:problem, contest: contest) }
+      let(:file_name) { "test.in" }
+      before do
+        expect(controller).to receive(:send_file).with(File.join(problem.tests_dir, file_name))
+        expect(controller).to receive(:render)
+
+        get :download_file, id: problem.id, contest_id: contest.id, file: file_name
+      end
+
+      it { is_expected.to respond_with(:success) }
+    end
+
+    describe "#compile_checker" do
+      let(:problem) { create(:problem, contest: contest) }
+      before { get :compile_checker, id: problem.id, contest_id: contest.id }
+
+      it { is_expected.to redirect_to(admin_contest_problem_path(contest, problem)) }
     end
   end
 
