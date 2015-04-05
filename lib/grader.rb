63d24a978dd126959de7597e9edb0df47545229c
Valentin Mihov <valentin.mihov@gmail.com>
2015-04-05 15:13:01 +0300

    Synchronize on the DB level when fetching runs for grading
    This change makes sure that concurrent graders work with different runs. This is done by DB level locking primitives.
    
    The code holds the locks for as little as possible, so that the workers are not blocked waiting.

--- lib/grader.rb
+++ lib/grader.rb
@@ -2,11 +2,11 @@ class Grader
   def initialize
     @tests_updated_at = SyncTests.new(Time.at(0)).call
   end
-  
+
   def run
     running = true
     puts "Ready to grade"
-    
+
     while running do
       ["INT", "TERM"].each do |signal|
         Signal.trap(signal) do
@@ -14,25 +14,29 @@ class Grader
           running = false
         end
       end
-      
+
       @tests_updated_at = SyncTests.new(@tests_updated_at).call
-      
-      if run = Run.where(:status => Run::WAITING).order("runs.created_at ASC").first
-        GradeRun.new(run).call
-      elsif run = Run.where(:status => Run::CHECKING).order("runs.created_at ASC").first
-        GradeRun.new(run, 1)
-      else
-        sleep 1
-      end
+
+      sleep 1 unless find_and_grade_run
+    end
+  end
+
+  def find_and_grade_run
+    if run = acquire_run(Run::WAITING)
+      GradeRun.new(run).call
+    elsif run = acquire_run(Run::CHECKING)
+      GradeRun.new(run, 1).call
+    else
+      false
     end
   end
 
   def benchmark(samples = 100)
     @tests_updated_at = SyncTests.new(@tests_updated_at).call
-    
+
     total_runs = 0
     total_factors = 0
-    
+
     total_factor = Run.where(:total_points => 100).where("max_time > 0").order("RAND()").limit(samples).each do |run|
       original_time = run.max_time
       original_points = run.total_points
@@ -41,16 +45,32 @@ class Grader
 
       run.update_time_and_mem
       run.update_total_points
-      
+
       next if original_points != run.total_points
-      
+
       total_factors += original_time / run.max_time
       total_runs += 1
     end
-    
+
     puts "The bech factor of the machine is #{total_factors / total_runs}"
   end
 
   private
-    attr_reader :tests_updated_at
+
+  attr_reader :tests_updated_at
+
+  def acquire_run(status)
+    run = Run.where(status: status).order(created_at: :asc).first
+
+    return unless run
+
+    Run.transaction do
+      run.lock!
+      return nil if run.status != status
+
+      run.update_attribute(:status, Run::JUDGING)
+
+      run
+    end
+  end
 end
