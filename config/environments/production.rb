9163613b7cbdd9ea3b5355882bc3db991cc32abd
Valentin Mihov <valentin.mihov@gmail.com>
2015-04-12 00:18:15 +0300

    Enable eager load, so that concurrency will be enabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ MaycampArena::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  config.eager_load = false
+  config.eager_load = true
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
