00e8c5714c07cdcf27a8b4991581fe2ae97e7502
Valentin Mihov <valentin.mihov@gmail.com>
2015-06-19 12:00:25 +0300

    This function is not used any more

--- lib/grader.rb
+++ lib/grader.rb
@@ -31,30 +31,6 @@ class Grader
     end
   end
 
-  def benchmark(samples = 100)
-    @tests_updated_at = SyncTests.new(@tests_updated_at).call
-
-    total_runs = 0
-    total_factors = 0
-
-    total_factor = Run.where(:total_points => 100).where("max_time > 0").order("RAND()").limit(samples).each do |run|
-      original_time = run.max_time
-      original_points = run.total_points
-
-      grade(run, nil, true)
-
-      run.update_time_and_mem
-      run.update_total_points
-
-      next if original_points != run.total_points
-
-      total_factors += original_time / run.max_time
-      total_runs += 1
-    end
-
-    puts "The bech factor of the machine is #{total_factors / total_runs}"
-  end
-
   private
 
   attr_reader :tests_updated_at
