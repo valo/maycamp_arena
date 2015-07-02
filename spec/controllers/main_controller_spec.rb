6fdd84381b8b709afe0b654e3e46d6a623c2eb7c
SValkanov <s.valkanov97@abv.bg>
2015-07-02 12:04:08 +0300

    main_controller_spec

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -1,5 +1,4 @@
 describe Admin::MainController do 
-  describe "upcoming contest create" do
     describe "#index" do
       it "should assign upcoming contest" do
         contest = create(:contest, start_time: 1.day.from_now, end_time: 2.days.from_now)
@@ -7,10 +6,4 @@ describe Admin::MainController do
         expect(assigns(:upcoming_contests)).to eq([contest])
       end
     end
-  end
 end
-
-
-git diff > mypatch.patch
-
-git diff playlist_spec.rb > Stanislav_Valkanov_A_25.patch
