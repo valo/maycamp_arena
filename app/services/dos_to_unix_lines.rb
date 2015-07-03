54df8f5c7128f7b19342cad8ec5d919cec982471
SValkanov <s.valkanov97@abv.bg>
2015-07-03 12:26:20 +0300

    fixing dos_to_unix_line_spec

--- app/services/dos_to_unix_lines.rb
+++ app/services/dos_to_unix_lines.rb
@@ -1,14 +1,14 @@
 class DosToUnixLines
-	def initialize source_filepath, destination_filepath
-		@source_filepath = source_filepath
-		@destination_filepath = destination_filepath
-	end
+  def initialize source_filepath, destination_filepath
+    @source_filepath = source_filepath
+    @destination_filepath = destination_filepath
+  end
 
-	def call
-		text = File.read(@source_filepath)
-		converted = text.gsub /\r\n?/, "\n"
-		File.open(@destination_filepath, 'w') { |file| file.write(converted) }
-	end
+  def call
+    text = File.read(@source_filepath)
+    converted = text.gsub /\r\n?/, "\n"
+    File.open(@destination_filepath, 'w') { |file| file.write(converted) }
+  end
 
 private
   attr_reader :source_filepath, :destination_filepath
--- spec/services/dos_to_unix_lines_spec.rb
+++ spec/services/dos_to_unix_lines_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 describe DosToUnixLines do
-	describe "#call" do
-		let(:global) { ActiveSupport::TestCase.fixture_path }
+  describe "#call" do
+    let(:global) { ActiveSupport::TestCase.fixture_path }
     let(:source) { File.join("#{global}", "/path1") }
     let(:destination) { File.join("#{global}", "/path2") }
-		let(:some) { DosToUnixLines.new(source, destination).call }
 
-		it "converts the new lines" do
-			converted = File.read(destination)
-			expect(converted).not_to include("\r\n")
-		end
-	end
+    it "converts the new lines" do
+      some = DosToUnixLines.new(source, destination).call
+      converted = File.read(destination)
+      expect(converted).not_to include("\r\n")
+    end
+    
+  end
 end
