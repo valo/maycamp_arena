69ed3826797d8119bd9255432431aa24aa076a7f
Stanislav Valkanov <s.valkanov97@abv.bg>
2015-07-16 00:55:34 +0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20150707154218) do
     t.datetime "updated_at"
     t.integer  "memory_limit",    limit: 4,                           default: 16777216
     t.string   "diff_parameters", limit: 255,                         default: "",       null: false
+    t.boolean  "runs_visible",    limit: 1
   end
 
   add_index "problems", ["contest_id"], name: "index_problems_on_contest_id", using: :btree
