637cee8ae60267d4d8d957129ae88a0a7d92b818
Симеон Ангелов <chepashnikow@gmail.com>
2015-12-07 02:34:09 +0200

    fix issue #108

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,52 @@
+describe UsersController do
+
+  shared_examples "accessed by unauthorized user" do
+    before { sign_in(current_user) if current_user }
+
+    describe "#edit" do
+      let(:user) { create(:user) }
+      before { get :edit, id: user.id }
+
+      it { is_expected.to redirect_to(new_session_path) }
+    end
+
+  end
+
+  shared_examples "accessed by authorized user" do
+    before { sign_in(current_user) }
+
+    describe "#new" do
+      before { get :new }
+
+      it { is_expected.to respond_with(:success) }
+    end
+
+    describe "#create" do
+      let(:user_params) { attributes_for(:user) }
+      before { post :create, user: user_params }
+
+      it { is_expected.to redirect_to(root_path) }
+    end
+
+    describe "#edit" do
+      let(:user) { create(:user) }
+      before { get :edit, id: user.id }
+
+      it { is_expected.to respond_with(:success) }
+    end
+  end
+
+
+  context "without a logged-in user" do
+    let(:current_user) { nil }
+
+    it_behaves_like "accessed by unauthorized user"
+  end
+
+  context "with an contester user" do
+    let(:current_user) { create(:user, role: User::CONTESTER) }
+
+    it_behaves_like "accessed by authorized user"
+  end
+end
+
