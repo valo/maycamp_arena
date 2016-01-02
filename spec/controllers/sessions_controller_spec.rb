492612fe3bdf05aef1695210c4a77149358fa0ff
Simeon Angelov <chepashnikow@gmail.com>
2016-01-03 00:21:16 +0200

    add spec for new authentication

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'pry'
+
+RSpec.describe SessionsController, type: :controller do
+
+  context 'with regular user' do
+    describe 'login' do
+
+      it 'set devise encryption and empty old password field' do
+        u = User.create(
+          login: "Valentin Mihov 123",
+          name: "login",
+          email: "valentin.mihov@gmail.com",
+          city: BG_CITIES.first,
+          encrypted_password: User.devise_encrypt_password("123")
+        )
+
+        u.unencrypted_password = "123"
+        u.save!
+
+        login = User.authenticate(u.email, u.password)
+
+        expect( login.password ).to eq nil
+      end
+    end
+  end
+end
