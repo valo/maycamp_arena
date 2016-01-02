37ac055d7e158571b08ef51cb5148a628109a707
Simeon Angelov <chepashnikow@gmail.com>
2016-01-02 21:15:34 +0200

    fix devise errors (https://github.com/plataformatec/devise/issues/558)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710140609) do
+ActiveRecord::Schema.define(version: 20151211212649) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -165,6 +165,16 @@ ActiveRecord::Schema.define(version: 20150710140609) do
   add_index "runs", ["status", "created_at"], name: "status_created_at", length: {"status"=>255, "created_at"=>nil}, using: :btree
   add_index "runs", ["updated_at"], name: "index_runs_on_updated_at", using: :btree
   add_index "runs", ["user_id", "problem_id"], name: "index_runs_on_user_id_and_problem_id", using: :btree
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", limit: 255,   null: false
+    t.text     "data",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "user_preferences", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
