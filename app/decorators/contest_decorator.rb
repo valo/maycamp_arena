7e41e709740d9164cb6ba205c28528dea83271f7
Valentin Mihov <valentin.mihov@gmail.com>
2016-01-09 16:08:27 +0200

    Adding some missing decorators

---
+++ app/decorators/contest_decorator.rb
@@ -0,0 +1,16 @@
+class ContestDecorator < Draper::Decorator
+  delegate_all
+
+  def best_practice_score
+    Run.where(:user_id => h.current_user.id).scoping do
+      problems.map do |problem|
+        # Find the max run for each problem
+        problem.runs.map(&:total_points).max
+      end.compact.sum
+    end
+  end
+
+  def practice_score_percent
+    best_practice_score * 100.0 / max_score
+  end
+end
