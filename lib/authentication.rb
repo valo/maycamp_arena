0bf0e476b56393d5b606e95a131da713453fce2d
Valentin Mihov <valentin.mihov@gmail.com>
2016-01-22 17:37:22 +0200

    Require user login when accessing the groups
    We need a logged in user so that we can render the statistics.

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -5,7 +5,7 @@ module Authentication
   def self.included(base)
     base.helper_method :current_user, :logged_in?
   end
-  
+
   protected
     # Returns true or false if the user is logged in.
     # Preloads @current_user with the user model if they're logged in.
@@ -18,7 +18,7 @@ module Authentication
     def current_user
       @current_user ||= login_from_session unless @current_user == false
     end
-    
+
     def current_user=(user)
       return unless user
       session[:user_id] = user.id
@@ -29,27 +29,27 @@ module Authentication
     def login_from_session
       self.current_user = User.find_by_id(session[:user_id]) if session[:user_id]
     end
-    
+
     def login_required_without_data_check
       session[:back] = nil
       if !logged_in?
         session[:back] = request.url if !logged_in?
-        redirect_to new_session_path
+        redirect_to login_path
       end
     end
-    
+
     def login_required
       login_required_without_data_check
       check_user_profile
     end
-    
+
     def check_user_profile
       if logged_in? and !current_user.valid?
         flash[:error] = "Данните в профила Ви не са пълни. Моле попълнете липсващите данни."
         redirect_to edit_user_path(current_user)
       end
     end
-    
+
     def logoff
       reset_session
     end
---
+++ spec/controllers/groups_controller_spec.rb
@@ -0,0 +1,25 @@
+describe GroupsController do
+  describe '#show' do
+    let(:contest) { create(:contest) }
+    let(:group) { contest.group }
+
+    context "without a signed in user" do
+      before do
+        get :show, id: group.id
+      end
+
+      it { is_expected.to redirect_to(login_path) }
+    end
+
+    context "with a signed in user" do
+      let(:user) { create(:user) }
+      before do
+        sign_in user
+
+        get :show, id: group.id
+      end
+
+      it { is_expected.to respond_with(:success) }
+    end
+  end
+end
