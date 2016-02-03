1023e6f29d6eec10f6fbc523bfb46894d7e855db
Simeon Angelov <chepashnikow@gmail.com>
2016-02-04 00:27:15 +0200

    clean old migration

--- db/migrate/20090911200430_add_admin_user.rb
+++ db/migrate/20090911200430_add_admin_user.rb
@@ -1,18 +1,17 @@
 class AddAdminUser < ActiveRecord::Migration
   def self.up
-    u = User.new(:login => 'root', 
-                 :name => "The admin", 
-                 :email => "valentin.mihov@gmail.com")
+    u = User.new(login: 'root',
+                 name:  'The admin',
+                 email: 'valentin.mihov@gmail.com')
 
-    u.unencrypted_password = "123123"
-    u.unencrypted_password_confirmation = "123123"
-    u.encrypted_password = User.devise_encrypt_password("123123") 
+    u.unencrypted_password = '123123'
+    u.unencrypted_password_confirmation = '123123'
     u.admin = true
-    u.city = "Sofia"
+    u.city = 'Sofia'
     u.save!
   end
 
   def self.down
-    User.destroy_all(:login => 'root')
+    User.destroy_all(login: 'root')
   end
 end
