2b385b70ce6a6ecd15b7eb9a8ecd5a74233009d2
Valentin Mihov <valentin.mihov@gmail.com>
2019-08-18 18:06:58 +0300

    Allow setting the site domain from the ENV
    Allow to build the app from the local source for local development.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ MaycampArena::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.default_url_options = { :host => "arena.maycamp.com" }
+  config.action_mailer.default_url_options = { :host => (ENV["SITE_DOMAIN"] || "arena.maycamp.com") }
 
   # Active Job config
   config.active_job.queue_adapter = :sidekiq
